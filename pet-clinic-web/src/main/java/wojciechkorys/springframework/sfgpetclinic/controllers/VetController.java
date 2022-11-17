d414ecd60ad468687293bc62dce0b8213e2b24a0
wojtek <wojtek.korys@wp.pl>
2022-11-18 00:04:15 +0100

    Added Vets List and updated controller/html

--- pet-clinic-web/src/main/java/wojciechkorys/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/wojciechkorys/springframework/sfgpetclinic/controllers/VetController.java
@@ -2,13 +2,23 @@ package wojciechkorys.springframework.sfgpetclinic.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wojciechkorys.springframework.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
