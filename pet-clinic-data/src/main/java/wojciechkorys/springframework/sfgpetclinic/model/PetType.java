0e0cfd4df63d3cb714757681f8b67158a9787832
Wojciech Koryś <wojtek.korys@wp.pl>
2022-12-17 21:13:49 +0100

    Convert Owners to JPA Entities

--- pet-clinic-data/src/main/java/wojciechkorys/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/wojciechkorys/springframework/sfgpetclinic/model/PetType.java
@@ -1,9 +1,14 @@
 package wojciechkorys.springframework.sfgpetclinic.model;
 
-import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
