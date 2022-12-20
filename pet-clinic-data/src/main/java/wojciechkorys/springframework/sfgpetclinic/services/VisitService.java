fc9e0270d5ea74a40b411fab9b05e2a19670a468
Wojciech Koryś <wojtek.korys@wp.pl>
2022-12-20 19:21:51 +0100

    Implemented Map impl for Visit Service

---
+++ pet-clinic-data/src/main/java/wojciechkorys/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package wojciechkorys.springframework.sfgpetclinic.services;
+
+import wojciechkorys.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
